c8bc5dfdf489b1b609a4a775bfd51f446f0974c4
SAGAR SATAPATHY <wishsagarks@gmail.com>
2022-02-14 03:36:36 +0530

    pics

--- App.js
+++ App.js
@@ -235,18 +235,18 @@ class CardCarousel extends DraggingEvent {
     let formula;
    
     if (x <= 0) {
-      formula = 1 - -1 / 5 * x
+      formula = 1 - -1 / 6 * x
       
       return formula
     } else if (x > 0) {
-      formula = 1 - 1 / 5 * x
+      formula = 1 - 1 / 6 * x
       
       return formula
     }
   }
   
   calcScale(x) {
-    const formula = 1 - 1 / 5 * Math.pow(x, 2)
+    const formula = 1 - 1 / 6 * Math.pow(x, 2)
     
     if (formula <= 0) {
       return 0 
@@ -288,7 +288,7 @@ class CardCarousel extends DraggingEvent {
     
     if (data != null) {
       this.container.classList.remove("smooth-return")
-      xDist = data.x / 250;
+      xDist = data.x / 260;
     } else {
 
       
