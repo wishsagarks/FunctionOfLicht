66714258566bdc5f555e58f0e97da7a0f5f38e0b
SAGAR SATAPATHY <32366752+wishsagarks@users.noreply.github.com>
2022-02-21 23:16:41 +0530

    Revert "added trial index"

--- exp/App.js
+++ exp/App.js
@@ -1,6 +1,7 @@
 // Swiper Configuration
 var swiper = new Swiper(".swiper-container", {
     slidesPerView: 1.5,
+    spaceBetween: 10,
     centeredSlides: true,
     freeMode: true,
     grabCursor: true,
@@ -13,16 +14,16 @@ var swiper = new Swiper(".swiper-container", {
       delay: 4000,
       disableOnInteraction: false
     },
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev"
-    // },
-    // breakpoints: {
-    //   500: {
-    //     slidesPerView: 1
-    //   },
-    //   700: {
-    //     slidesPerView: 1.5
-    //   }
-    // }
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 1
+      },
+      700: {
+        slidesPerView: 1.5
+      }
+    }
   });
